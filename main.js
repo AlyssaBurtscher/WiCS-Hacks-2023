5a1a6b9da79b237817d23e97aa02aec6bb59df1c
Yangli Esther Peng <pengesther1@gmail.com>
2023-02-04 18:56:09 -0600

    completed basic function of problems page

--- main.js
+++ main.js
@@ -14,7 +14,36 @@ function one(){
         window.location.href = "problems.html";   
     } 
 }
+var pushes = 0;
+var y = 0;
+var x =0;
 
+function submitp(){
+    pushes++;
+    console.log(pushes);
+    x = document.getElementById("Questions").value;
+    console.log(x);
+    let hundred = 100;
+    let percentage = 100/x;
+    if(pushes<=x){
+        //add percentage to 0
+        y = y+percentage;
+        if(y>99){
+            y=100;
+            console.log("congrats, you're done!");
+
+        }
+        console.log(y + "% done");
+    }
+    document.getElementById("objectId").innerHTML = y + "% done";
+
+}
+
+function reset(){
+    pushes = 0;
+    x = 0;
+    y=0;
+}
 function back(){
     window.location.href = "index.html";
 }
