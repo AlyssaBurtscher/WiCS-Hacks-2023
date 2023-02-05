790b9d278bdec0723c33f20ab6a417c0f842c3e9
AlyssaBurtscher <alyssa.burtscher@utexas.edu>
2023-02-04 23:59:25 -0600

    Fix Some Things

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
 var assignments = ["One", "Two", "Three"];
 
-class assignment (){
+class ImplementAssignments{
+    constructor() {
+        let newAssignments = new Map();
+        var obj = Object.fromEntries(map);
+        var jsonString = JSON.stringify(obj);
+    }
+    
+    addAssignment() {
+        switch(assignment) {
+            case "Essay":
+                let words = Numbers(prompt("Number of words: "));
+                let revisions = Numbers(prompt("Number of revisions: "));
+                newAssignments.set(name, new essay(0, name, dueDate, words, revisions));
+                break;
+            case "Quiz":
+                let studyTime = Numbers(prompt("Time to study: "));
+                let quizLength = Numbers(prompt("Length of quiz: "));
+                newAssignments.set(name, new quiz(0, name, dueDate, studyTime, quizLength));
+                break;
+            case "Problem Set":
+                let numProblems = Numbers(prompt("Time to study: "));
+                newAssignments.set(name, new problems(0, name, dueDate, numProblems));
+                break;
+            case "Task":
+                newAssignments.set(name, new problems(0, name, dueDate));
+                break;
+        }
+        obj = Object.fromEntries(map);
+        jsonString = JSON.stringify(obj.toString());
+    }
+    
+    toString() {
+        return jsonString;
+    }
+}
+
+class Assignment{
     constructor(percentDone, name, dueDate) {
         this.percentDone = percentDone;
         this.name = name;
@@ -8,30 +44,123 @@ class assignment (){
     }
     
     completed() {
-        return percentDone == 100;
+        return this.percentDone == 100;
     }
     
     getPercentage() {
-        return percentDone;
+        return this.percentDone;
     }
     
     getName() {
-        return name;
+        return this.name;
     }
     
     tracking() {
-        percentDone = 100;
+        this.percentDone = 100;
     }
     
     getDueDate() {
-        return dueDate;
+        return this.dueDate;
     }
     
     toString() {
-        return "\nTitle: " + name + "\nDue Date: " + dueDate + "\nPercent Done: " + percentDone;
+        return "\nTitle: " + this.name + "\nDue Date: " + this.dueDate + "\nPercent Done: " + this.percentDone;
     }
-};
+}
 
+class Essay extends Assignment{
+    constructor(percentDone, name, dueDate, words, revisions){
+        super(percentDone, name, dueDate);
+        this.words = words;
+        this.wordsWritten = 0.0;
+        this.revisions = revisions;
+        this.revisionsDone = 0.0;
+    }
+    
+    getWordsDone() {
+        return this.wordsWritten;
+    }
+    
+    wordsAdded(numWordsAdded) {
+        this.wordsWritten = numWordsAdded;
+    }
+    
+    getRevisionsDone() {
+        return this.revisionsDone;
+    }
+    
+    revisionCompleted() {
+        this.revisionsDone++;
+    }
+    
+    getMaxRevisions() {
+        return this.revisions;
+    }
+    
+    tracking() {
+        this.percentDone = ((this.wordsWritten / this.words) * 80) + ((this.revisionsDone / this.revisions) * 20);
+    }
+    
+    toString() {
+        return super.toString() + "\nWords Written: " + this.wordsWritten + "\nRequired Word Count: " + this.words + "\nRevisions Done: " + this.revisionsDone + "\nRevisions Required: " + revisions;
+    }
+}
+
+class Quiz extends Assignment{
+    constructor(percentDone, name, dueDate, studyTime, lengthOfQuiz){
+        super(percentDone, name, dueDate);
+        this.studyTime = studyTime;
+        this.studyDone = 0.0;
+    }
+    
+    getLengthOfQuiz() {
+        return this.lengthOfQuiz;
+    }
+    
+    getTimeStudies() {
+        return this.studyDone;
+    }
+    
+    increaseTimeStudied(addTime) {
+        this.studyDone += addTime;
+    }
+    
+    tracking() {
+        this.percentDone = (studyDone / studyTime) * 100;
+    }
+    
+    toString() {
+        return super.toString() + "\nTime Studied: " + this.studyDone + "\nTotal Time To Study: " + this.studyTime + "\nLength of Quiz: " + this.lengthOfQuiz;
+    }
+}
+
+class Problems extends Assignment{
+    constructor(percentDone, name, dueDate, numProblems){
+        super(percentDone, name, dueDate);
+        this.numProblems = numProblems;
+        this.problemsDone = 0.0;
+    }
+    
+    getnumProblems() {
+        return this.numProblems;
+    }
+    
+    getProblemsDone() {
+        return this.problemsDone;
+    }
+    
+    changeProblemsDone() {
+        this.problemsDone++;
+    }
+    
+    tracking() {
+        this.percentDone = (problemsDone / numProblems) * 100;
+    }
+    
+    toString() {
+        return super.toString() + "\nProblems Completed: " + problemsDone + "\nTotal Number of Problems: " + numProblems;
+    }
+}
 function one(){
     var e = document.getElementById("assignment");
     var value = e.value;
@@ -79,6 +208,7 @@ function reset(){
     x = 0;
     y=0;
 }
+
 function back(){
     window.location.href = "index.html";
 }
@@ -86,8 +216,7 @@ function back(){
 function submit(){
     var title = document.getElementById("Title").value;
     var dueDate = document.getElementById("dueDate").value;
-    //import {assignment} from './classesFile.js';
-    var assignment = new assignment(0, title, dueDate);
+    var assignment = new Assignment(0, title, dueDate);
     assignments.push(assignment.getName() + " " + assignment.getDueDate());
 }
 
